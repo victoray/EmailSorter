a513d678785ca28a0e28180e62a4e421d69aa694
Victor A <viktoray007@gmail.com>
2019-06-25 14:56:41 -0700

    fix total runtime

--- sorter.py
+++ sorter.py
@@ -4,56 +4,55 @@ import dns.resolver
 from time import time, sleep
 
 start_time = time()
-
+unmatched = set()
 with open('emails.txt', 'r') as file:
-    for line in file:
-        domain = line.split('@')[1].strip('\n').split('.')
+    emails = set([line for line in file])
 
-        if 'gmail' in domain:
-            txt = 'google'
-        elif 'yahoo' in domain or 'aol' in domain:
-            txt = 'yahoo'
-        elif 'outlook' in domain or 'hotmail' in domain:
-            txt = 'outlook'
-        elif 'comcast' in domain:
-            txt = 'comcast'
-        else:
-            txt = 'none'
+print(len(emails))
+for email in emails:
+    domain = email.split('@')[1].strip('\n').split('.')
 
+    if 'gmail' in domain:
+        txt = 'google'
+    elif 'yahoo' in domain or 'aol' in domain:
+        txt = 'yahoo'
+    elif 'outlook' in domain or 'hotmail' in domain:
+        txt = 'outlook'
+    elif 'comcast' in domain:
+        txt = 'comcast'
+    else:
+        unmatched.add(email)
+
+    try:
         with open('{}.txt'.format(txt), 'a') as f:
-            f.write(line)
+            f.write(email.strip(" "))
+    except:
+        pass
 
-try:
-    with open('none.txt', 'r') as file:
-        for line in file:
-            domain = line.split('@')[1].strip('\n')
-            try:
-                answers = dns.resolver.query(domain, 'MX')
-                exchange = str(answers[0].exchange).split('.')
-                print(exchange)
-            except:
-                exchange = ""
-                traceback.print_exc()
+print(len(unmatched))
+for email in unmatched:
+    domain = email.split('@')[1].strip('\n')
+    try:
+        answers = dns.resolver.query(domain, 'MX')
+        exchange = str(answers[0].exchange).split('.')
+        print(exchange)
+    except:
+        exchange = ""
+        traceback.print_exc()
 
-            if 'google' in exchange:
-                txt = 'google'
-            elif 'yahoo' in exchange or 'aol' in exchange:
-                txt = 'yahoo'
-            elif 'outlook' in exchange or 'hotmail' in exchange:
-                txt = 'outlook'
-            elif 'comcast' in exchange:
-                txt = 'comcast'
-            else:
-                txt = 'other'
+    if 'google' in exchange:
+        txt = 'google'
+    elif 'yahoo' in exchange or 'aol' in exchange:
+        txt = 'yahoo'
+    elif 'outlook' in exchange or 'hotmail' in exchange:
+        txt = 'outlook'
+    elif 'comcast' in exchange:
+        txt = 'comcast'
+    else:
+        txt = 'other'
 
-            with open('{}.txt'.format(txt), 'a') as f:
-                f.write(line)
-except FileNotFoundError:
-    pass
-
-
-def writer(email, file, exchange):
-    pass
+    with open('{}.txt'.format(txt), 'a') as f:
+        f.write(email.strip(" "))
 
 # program end time
 end_time = time()
